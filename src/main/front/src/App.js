006e36cfeb2dcabf91330e1fc20ab0df93cdcef2
maeng17 <soon5796735@naver.com>
2024-05-16 14:20:32 +0900

    1

---
+++ src/main/front/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>Spring Boot + React + NCP Test!!</h1>
+
+      {/* public을 기준으로 */}
+      <img src='./image/image04.jpg' alt='cat' /> 
+    </div>
+  );
+};
+
+export default App;
